eba1d2a05611f525cb3d4e4f313eda0ea59422a7
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-09-06 17:42:34 +0500

    Update

--- daoist/urls.py
+++ daoist/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'daoist.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', 'blog.views.home_view', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),

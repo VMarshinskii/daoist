10cdbdf77d7172109de24008966b9c481fbfa19a
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-09-06 19:46:54 +0500

    Update

--- consultations/admin.py
+++ consultations/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from consultations.models import Consultation
 
-admin.site.register(Consultation)
+class ConsultationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"url": ("title",)}
+
+admin.site.register(Consultation, ConsultationAdmin)
+
--- consultations/models.py
+++ consultations/models.py
@@ -7,8 +7,8 @@ class Consultation(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     description = models.CharField("Описание", max_length=200, blank=True)
     keyword = models.CharField("Ключевые слова", max_length=200, blank=True)
-    question = models.TextField(verbose_name="Вопрос")
-    reply = RedactorField(verbose_name="Ответ", redactor_options={'upload_to': 'static/uploads'}, blank=True)
+    text = RedactorField(verbose_name="Ответ", redactor_options={'upload_to': 'static/uploads'}, blank=True)
+    url = models.CharField("Url", max_length=200, unique=True)
     public = models.BooleanField("Опубликованно", default=False)
     date_time = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
 

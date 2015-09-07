ecbb96c73745101d24cb51a5b58daf5ff6caa842
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-09-07 15:25:11 +0500

    Update

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, Http404
 from models import Page
+from my_admin.models import SiteSettings
 
 
 def page_view(request, url):
@@ -7,11 +8,19 @@ def page_view(request, url):
         page = Page.objects.get(url=url, public=True)
         return render_to_response("page.html", {'page': page})
     except Page.DoesNotExist:
-        raise Http404
+        raise Http404()
 
 
 def about_view(request):
-    return render_to_response("about.html")
+    try:
+        settings = SiteSettings.objects.get(id=1)
+        return render_to_response("about.html", {'settings': settings})
+    except SiteSettings.DoesNotExist:
+        raise Http404()
 
 def contacts_view(request):
-    return render_to_response("contacts.html")
+    try:
+        settings = SiteSettings.objects.get(id=1)
+        return render_to_response("contacts.html", {'settings': settings})
+    except SiteSettings.DoesNotExist:
+        raise Http404()
